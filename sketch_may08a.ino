1ead4a9a7f4d350fee23035794b90c28296e4034
ShellyBoden <43201167+ShellyBoden@users.noreply.github.com>
2019-05-08 11:33:51 +0800

    Add files via upload

---
+++ sketch_may08a.ino
@@ -0,0 +1,20 @@
+int btn = 2;
+int led = 13;
+int btnState=0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led,OUTPUT);
+  pinMode(btn,INPUT);
+  digitalWrite(btn,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  btnState = digitalRead(btn);
+  if(btnState==HIGH){
+    digitalWrite(led,LOW);
+  }
+  else{
+    digitalWrite(led,HIGH);
+  }
+}
